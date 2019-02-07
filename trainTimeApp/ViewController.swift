a93227ca54a0a67343b0500f79d100310b8d56cf
洪丞桀 <jaychung@hongchengjiede-macbook-pro.local>
2019-02-07 13:36:16 +0800

    Initial Commit

---
+++ trainTimeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainTimeApp
+//
+//  Created by 洪丞桀 on 2019/2/7.
+//  Copyright © 2019 JayChung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
